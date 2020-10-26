96964cf378a4bdf38ef38d79bf1f05394c7dcf1a
Yu-Shiang <yushaingwtw@redone.com>
2020-10-26 12:53:11 +0000

    ..

--- makecmds.sh
+++ makecmds.sh
@@ -11,15 +11,15 @@ ln -s ../deps/Mask_RCNN ./ || true
 #  -DCUDA_HOST_COMPILER=/usr/bin/gcc \
 #  -DWITH_FREENECT2=OFF \
 #  ..
-
+#  -DMASKFUSION_PYTHON_VE_PATH="/media/ysdev/mycode/my-maskfusion/python-environment" \
+#  
 
 cmake \
-	  -DOpenCV_DIR="/media/ysdev/mycode/my-maskfusion/deps/opencv/build" \
-	    -DPangolin_DIR="/media/ysdev/mycode/my-maskfusion/deps/Pangolin/build/src" \
-	      -DMASKFUSION_PYTHON_VE_PATH="/media/ysdev/mycode/my-maskfusion/python-environment" \
-	        -DCUDA_HOST_COMPILER=/usr/bin/gcc \
-		  -DWITH_FREENECT2=OFF \
-		  ..
+	  -DOpenCV_DIR="../deps/opencv/build" \
+	  -DPangolin_DIR="../deps/Pangolin/build/src" \
+	  -DCUDA_HOST_COMPILER=/usr/bin/gcc \
+	  -DWITH_FREENECT2=OFF \
+	   ..
 		   # -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m \
 		   #   -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so.1 \
 
